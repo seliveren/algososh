2ada9e9d0011c95f85c22da216337d131d7722b2
Yulia Selivanova <selivanyulia@mail.ru>
2023-05-07 12:11:07 +0300

    feat: add algorithms

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,132 @@
 import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import StackPageStyles from "../stack-page/stack-page.module.css";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import {Circle} from "../ui/circle/circle";
+import {ElementStates} from "../../types/element-states";
 
-export const StackPage: React.FC = () => {
-  return (
-    <SolutionLayout title="Стек">
 
-    </SolutionLayout>
-  );
-};
+interface IStack<T> {
+  push: (item: T) => void;
+  pop: () => void;
+  peak: () => T | null;
+  getSize: () => number;
+}
+
+export class StackPage<T> extends React.Component<{}, { value: number | null, change: number, colour: ElementStates }> implements IStack<number> {
+
+  private container: number[] = [];
+
+  constructor(props: any) {
+    super(props)
+    this.state = {value: null, change: 0, colour: ElementStates.Default};
+    this.handleChange = this.handleChange.bind(this);
+  };
+
+
+  handleChange(event: React.FormEvent<HTMLInputElement>) {
+    this.setState({value: Number((event.target as HTMLInputElement).value)});
+  };
+
+
+  push = (item: number): void => {
+    if (item !== null) {
+      this.container.push(item)
+      this.setState({change: 1});
+      this.setState({colour: ElementStates.Changing});
+      setTimeout(() => this.setState({colour: ElementStates.Default}), 500)
+    }
+  };
+
+
+  pop = (): void => {
+    if (this.container.length > 0) {
+      this.setState({change: 0});
+      this.setState({colour: ElementStates.Changing});
+      setTimeout(() => {
+        this.container.pop();
+        this.setState({colour: ElementStates.Default});
+      }, 500)
+    }
+  };
+
+
+  peak = (): number | null => {
+    if (this.container.length > 0) {
+      return this.container[this.container.length - 1];
+    }
+    return null;
+  };
+
+
+  getSize = () => this.container.length;
+
+
+  clear = (): void => {
+    this.setState({change: -1});
+    if (this.container.length > 0) {
+      while (this.container.length > 0) {
+        this.container.pop();
+      }
+    }
+  };
+
+
+  handleSubmit(e: { preventDefault: () => void; }) {
+    e.preventDefault();
+    console.log('submitted!');
+    (document.getElementById("input") as HTMLInputElement).value = '';
+    this.setState({value: null});
+  };
+
+
+  render() {
+
+    return (
+      <SolutionLayout title="Стек">
+
+        <form className={StackPageStyles.mainContainer} onSubmit={this.handleSubmit}>
+          <Input placeholder={"Введите значение"} extraClass={StackPageStyles.inputField} id="input" type={"number"}
+                 isLimitText={true} max={4} onChange={(event) => {
+            this.handleChange(event)
+          }}/>
+
+          {
+            this.state.value !== null && (document.getElementById("input") as HTMLInputElement).value.length < 5 ?
+              <Button extraClass={StackPageStyles.addButton} id={"addButton"} type={'submit'} text={'Добавить'} onClick={(e) => {
+                this.push(this.state.value as number);
+                this.handleSubmit(e);
+              }}/>
+              :
+              <Button extraClass={StackPageStyles.addButton} id={"addButton"} type={'submit'} text={'Добавить'} disabled={true}/>
+          }
+
+          {this.getSize() > 0 ?
+            <><Button extraClass={StackPageStyles.deleteButton} id={"deleteButton"} text={'Удалить'} onClick={() => this.pop()}/>
+              <Button extraClass={StackPageStyles.clearButton} text={'Очистить'} onClick={() => {
+                this.clear()
+              }}/></>
+            :
+            <><Button extraClass={StackPageStyles.deleteButton} id={"deleteButton"} text={'Удалить'} onClick={() => this.pop()} disabled={true}/>
+              <Button extraClass={StackPageStyles.clearButton} text={'Очистить'} onClick={() => {
+                this.clear()
+              }} disabled={true}/></>
+          }
+        </form>
+
+        <div className={StackPageStyles.resultContainer}>
+          {
+            this.container.map((el: number, index: number) =>
+              index == this.getSize() - 1 ?
+                <Circle state={this.state.colour} key={index} letter={JSON.stringify(el)} index={index}
+                        head={'top'}/> :
+                <Circle state={ElementStates.Default} key={index} letter={JSON.stringify(el)} index={index}/>
+            )
+          }
+        </div>
+
+      </SolutionLayout>
+    );
+  }
+}
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,95 @@
 import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import StringPageStyles from "../string/string.module.css";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import {Circle} from "../ui/circle/circle";
+import {ElementStates} from "../../types/element-states";
 
 export const StringComponent: React.FC = () => {
+
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [value, setValue] = React.useState<string[]>([]);
+  const [marker, setMarker] = React.useState<boolean>(false);
+  const [initialColor, setInitialColor] = React.useState<boolean>(true);
+  const [iteration, setIteration] = React.useState<number>();
+  const [startIndex, setStartIndex] = React.useState<number | null>();
+  const [endIndex, setEndIndex] = React.useState<number | null>();
+
+  React.useEffect(() => {
+    document.getElementById("input")!.addEventListener(("change"), () => {
+      setValue((document.getElementById("input") as HTMLInputElement)!.value.split(''));
+      setInitialColor(true);
+      setIteration(0);
+    });
+  }, [])
+
+  const swap = (arr: string[], firstIndex: number, secondIndex: number): void => {
+    const temp = arr[firstIndex];
+    arr[firstIndex] = arr[secondIndex];
+    arr[secondIndex] = temp;
+  };
+
+  function swapString() {
+    let start = 0;
+    let end = value.length - 1;
+    const mid = Math.floor((start + end) / 2)
+    setInitialColor(false)
+
+    for (let i = 0; i <= mid; i++) {
+      setTimeout(() => {
+          swap(value, end, start);
+          setIteration(i);
+          setStartIndex(start);
+          setEndIndex(end);
+          start++;
+          end--;
+        },
+        1000 * i);
+    }
+
+    setTimeout(() => {
+      setLoading(false);
+      setStartIndex(null);
+      setEndIndex(null);
+      setIteration(-1);
+    }, 500 * (value.length - 1) + 50)
+  }
+
+
+  function onSubmit(e: { preventDefault: () => void; }) {
+    e.preventDefault();
+    if (value.length <= 11 && value.length > 0) {
+      setMarker(true);
+      setTimeout(() => setLoading(true), 700);
+      setTimeout(() => swapString(), 1000);
+    }
+  }
+
+
   return (
     <SolutionLayout title="Строка">
-     
+
+      <form id="form" className={StringPageStyles.mainContainer} onSubmit={onSubmit}>
+        <Input type={"text"} id="input" isLimitText={true} maxLength={11}/>
+        {(value.length > 11 || value.length < 1) ?
+          <Button extraClass={StringPageStyles.button} text={"Развернуть"} type={"submit"} id="button" disabled={true}/>
+          : <Button extraClass={StringPageStyles.button} text={"Развернуть"} type={"submit"} id="button"
+                    isLoader={loading}/>}
+      </form>
+
+      <div className={StringPageStyles.resultContainer}>
+        {marker ?
+          value.map((el: any, index: number) =>
+            loading && (startIndex == index || endIndex == index) ?
+              <Circle state={ElementStates.Changing} key={index} letter={el} index={index}/> : !initialColor && (
+                startIndex! >= index || endIndex! <= index) || iteration == -1 ?
+                <Circle state={ElementStates.Modified} key={index} letter={el} index={index}/> :
+                <Circle state={ElementStates.Default} key={index}
+                        letter={el} index={index}/>)
+          : <></>}
+      </div>
+
     </SolutionLayout>
   );
 };
